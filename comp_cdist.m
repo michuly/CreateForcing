b31fb938ebf6f7e3a022528aa2b9a6763b63288f
michalshaham89 <michalshaham@mail.tau.ac.il>
2023-11-01 16:24:14 +0200

    initial edit

--- comp_cdist.m
+++ comp_cdist.m
@@ -30,42 +30,39 @@ for it = 1:1
     ncx = nch(it);
     ncy = nch(it);
     for ic = 1:ncx
-      for jc = 1:ncy
-        [ic jc]
-        i0 = 1 + (ic-1)*ceil(Lp/ncx);
-        i1 = i0+ceil(Lp/ncx) + 20;
-        j0 = 1 + (jc-1)*ceil(Mp/ncy);
-        j1 = j0+ceil(Mp/ncy) + 20;
-        i1 = min(i1,Lp); j1 = min(j1,Mp);
-        lons = lon(j0:j1,i0:i1);
-        lats = lat(j0:j1,i0:i1);
-        masks= mask(j0:j1,i0:i1);
-        lab = 0*masks + 1; lab(2:end-1,2:end-1) = masks(1:end-2,2:end-1)+masks(3:end,2:end-1)+masks(2:end-1,1:end-2)+masks(2:end-1,3:end);
-        mlon = lons;mlon(masks>0|lab<1) = [];
-        mlat = lats;mlat(masks>0|lab<1) = [];
-        disp('here')
-        swt = sum(~isnan(mlon))>0
-    
-        if sum(~isnan(mlon))>0
-            for j = j0:j1
-                [j j1]
-                for i = i0:i1
-                    if mask(i,j) < 1 
-                      cdist(i,j) = 0;
-                    else
-                      dist =  gc_dist(lon(j,i)*d2r,lat(j,i)*d2r,mlon*d2r,mlat*d2r);
-                      mdist = min(min(dist));
-                      cdist(i,j) = min(mdist,cdist(i,j));
+        for jc = 1:ncy
+            [ic jc]
+            i0 = 1 + (ic-1)*ceil(Lp/ncx);
+            i1 = i0+ceil(Lp/ncx) + 20;
+            j0 = 1 + (jc-1)*ceil(Mp/ncy);
+            j1 = j0+ceil(Mp/ncy) + 20;
+            i1 = min(i1,Lp); j1 = min(j1,Mp);
+            lons = lon(j0:j1,i0:i1);
+            lats = lat(j0:j1,i0:i1);
+            masks= mask(j0:j1,i0:i1);
+            lab = 0*masks + 1; lab(2:end-1,2:end-1) = masks(1:end-2,2:end-1)+masks(3:end,2:end-1)+masks(2:end-1,1:end-2)+masks(2:end-1,3:end);
+            mlon = lons;mlon(masks>0|lab<1) = [];
+            mlat = lats;mlat(masks>0|lab<1) = [];
+            disp('here')
+            swt = sum(~isnan(mlon))>0
+            
+            if sum(~isnan(mlon))>0
+                for j = j0:j1
+                    [j j1]
+                    for i = i0:i1
+                        if mask(i,j) < 1 
+                          cdist(i,j) = 0;
+                        else
+                          dist =  gc_dist(lon(j,i)*d2r,lat(j,i)*d2r,mlon*d2r,mlat*d2r);
+                          mdist = min(min(dist));
+                          cdist(i,j) = min(mdist,cdist(i,j));
+                        end
                     end
                 end
+            else
+            cdist(j0:j1,i0:i1) = min(3e5,cdist(j0:j1,i0:i1));
             end
-        else
-        cdist(j0:j1,i0:i1) = min(3e5,cdist(j0:j1,i0:i1));
-    
-        
-        
         end
-      end
     end
 end
 
--- fill_frc_era.m
+++ fill_frc_era.m
@@ -16,25 +16,25 @@
 %
 %  frc climatology file names:
 
-frc_dir = '/paracas/DATASETS/ERA5/';
+frc_dir = '/atlantic3/ROMS_UCLA_DATA_FILES/ERA5/';
 
 % Set a date range for the forcing file
-start_date = datenum(2012,11,1);
-end_date   = datenum(2013,4,1);
+start_date = datenum(2012,1,1);
+end_date   = datenum(2012,4,1);
 
-grdname = '/paracas/ROMS_SOLUTIONS/SWELL/nepac4km_grd.nc';
-disname = '/paracas/ROMS_SOLUTIONS/SWELL/nepac4km_cdist.mat';
+grdname = '/atlantic3/rbarkan/EPAC2km/Epac2km.nc';
+disname = '/atlantic2/michalshaham/Forcing/Epac2km_cdist.mat';
 % a rivname is needed if add_rivers = 1 ; it needs to be generated previously 
 rivname = '/paracas/nmolem/NEPAC/nepac_swf.nc';
 
 swcorrname = [frc_dir 'SSR_correction.nc'];
 
-root_name = '/paracas/ROMS_SOLUTIONS/SWELL/nepac4km';
+root_name = '/atlantic2/michalshaham/Forcing/Epac2km';
 
 coarse_frc   = 0; % forcing files at half the resolution of the grid
 wind_dropoff = 1; 
 add_rivers   = 0; % Adds river runoff as additional precipitation
-                  % We don't usually do this anymore!
+              % We don't usually do this anymore!
 
 %
 %%%%%%%%%%%%%%%%%%% END USER-DEFINED VARIABLES %%%%%%%%%%%%%%%%%%%%%%%
@@ -46,8 +46,8 @@ nfiles = length(eralist);
 % find the right files
 stimes = zeros(nfiles,1);
 for i=1:nfiles
- datname = [frc_dir eralist(i).name];
- stime(i) = double(ncread(datname,'time',[1],[1]))/24 + datenum(1900,1,1);
+    datname = [frc_dir eralist(i).name];
+    stime(i) = double(ncread(datname,'time',[1],[1]))/24 + datenum(1900,1,1);
 end
 t0 = find(stime<start_date,1,'last');
 t1 = find(stime>end_date,1,'first');
@@ -60,13 +60,13 @@ nfiles = length(eralist);
 disp(' ')
 disp(' Read in the target grid')
 if coarse_frc
- lon = ncread(grdname,'lon_coarse');
- lat = ncread(grdname,'lat_coarse');
- ang = ncread(grdname,'angle_coarse');
+    lon = ncread(grdname,'lon_coarse');
+    lat = ncread(grdname,'lat_coarse');
+    ang = ncread(grdname,'angle_coarse');
 else
- lon = ncread(grdname,'lon_rho');
- lat = ncread(grdname,'lat_rho');
- ang = ncread(grdname,'angle');
+    lon = ncread(grdname,'lon_rho');
+    lat = ncread(grdname,'lat_rho');
+    ang = ncread(grdname,'angle');
 end
 [nx,ny] = size(lon);
 
@@ -109,176 +109,176 @@ mask = fliplr(mask); % to deal with upside down ERA5 data
 
 
 if wind_dropoff
- if ~exist(disname)
-   comp_cdist
- else
-   load(disname)
- end
- cdist = cdist/1e3;
-%    mult = 1-0.6*exp(-0.08*cdist);
- mult = 1-0.4*exp(-0.08*cdist);
+    if ~exist(disname)
+        comp_cdist
+    else
+        load(disname)
+    end
+    cdist = cdist/1e3;
+    %    mult = 1-0.6*exp(-0.08*cdist);
+    mult = 1-0.4*exp(-0.08*cdist);
 end
 
 % prepare for short wave radiation correction
 corr_time = ncread(swcorrname,'time');
 swr_mult = zeros(nx,ny,12);
 for i=1:12
- corr_swr  = ncread(swcorrname,'ssr_corr',[i0 j0 i],[fnx fny 1]); 
- corr_swr = fliplr(corr_swr);
- corr_swr(mask<1) = nan;
- corr_swr = inpaint_nans(corr_swr,2);
- swr_mult(:,:,i) = interp2(lon_frc,lat_frc,corr_swr',lon,lat,'makima');
+    corr_swr  = ncread(swcorrname,'ssr_corr',[i0 j0 i],[fnx fny 1]); 
+    corr_swr = fliplr(corr_swr);
+    corr_swr(mask<1) = nan;
+    corr_swr = inpaint_nans(corr_swr,2);
+    swr_mult(:,:,i) = interp2(lon_frc,lat_frc,corr_swr',lon,lat,'makima');
 end
 
 if add_rivers
- % prepare for the addition of river runoff to rain data
- riv_time = ncread(rivname,'swf_time');
- riv_flux = ncread(rivname,'swf_flux');
+    % prepare for the addition of river runoff to rain data
+    riv_time = ncread(rivname,'swf_time');
+    riv_flux = ncread(rivname,'swf_flux');
 end
 
 
 
 % Loop over data files
 for i = 1:nfiles
- datname = [frc_dir eralist(i).name];
- disp([' Processing: ' eralist(i).name])
-
- dat_time = ncread(datname,'time');
- nrecord = length(dat_time);
-
- date_num = double(dat_time(1)/24) + datenum(1900,1,1);
- label = datestr(date_num,'YYYYmm')
-
- frcname = [root_name '_frc_' label '.nc'] 
-
- data.datname = datname;
- data.lon = lon_frc;
- data.lat = lat_frc;
- data.i0 = i0;
- data.j0 = j0;
- data.fnx = fnx;
- data.fny = fny;
- data.mask = mask;
-
- if exist(frcname)
-   delete(frcname)
- end
- create_frc_bulk(grdname,frcname);
-
- for irec = 1:nrecord
-    disp(['Record: ' num2str(irec)])
-
-% ---- time -----
-time = ncread(datname,'time',[irec],[1]);
-
-% translate to days since 2000,1,1
-    days = double(time)/24. + datenum(1900,1,1) - datenum(2000,1,1) 
-ncwrite(frcname,'time',days,[irec]);
-
-% ---- 10 meter winds -----
-u = get_frc_era(data,grd,'u10',irec,'makima');
-v = get_frc_era(data,grd,'v10',irec,'makima');
-
-% rotate to grid angles 
-    ugrid = cosa.*u + sina.*v;
-    vgrid =-sina.*u + cosa.*v;
-
-if wind_dropoff
-      ugrid = ugrid.*mult;
-      vgrid = vgrid.*mult;
+    datname = [frc_dir eralist(i).name];
+    disp([' Processing: ' eralist(i).name])
+    
+    dat_time = ncread(datname,'time');
+    nrecord = length(dat_time);
+    
+    date_num = double(dat_time(1)/24) + datenum(1900,1,1);
+    label = datestr(date_num,'YYYYmm')
+    
+    frcname = [root_name '_frc_' label '.nc'] 
+    
+    data.datname = datname;
+    data.lon = lon_frc;
+    data.lat = lat_frc;
+    data.i0 = i0;
+    data.j0 = j0;
+    data.fnx = fnx;
+    data.fny = fny;
+    data.mask = mask;
+    
+    if exist(frcname)
+        delete(frcname)
     end
-
-ncwrite(frcname,'uwnd',ugrid,[1 1 irec]);
-ncwrite(frcname,'vwnd',vgrid,[1 1 irec]);
-
-% ---- Incoming Radiation -----
-swr = get_frc_era(data,grd,'ssr',irec,'linear');  % downward_shortwave_flux [J/m2]
-lwr = get_frc_era(data,grd,'strd',irec,'linear'); % downward_longwave_flux [J/m2]
-
-% Translate to fluxes. ERA5 stores values integrated over 1 hour
-% Are these centered around the current time? 
-swr = swr/3600;
-lwr = lwr/3600;
-
-% ---- Correction to swr using  COREv2 dataset -----
-% This is a multiplicative correcting, trying to account for 
-% for errors in the ERA5 cloud cover
-% temporal interpolation in a monthly climatology
-yr_day = mod(days,365.25);
-if yr_day>=corr_time(12)
-       id0 = 12;
-       id1 =  1;
-   cf  = (yr_day - corr_time(12))/(corr_time(1)+365.25-corr_time(12));
-    elseif yr_day<=corr_time(1)
-       id0 = 12;
-       id1 =  1;
-   cf  = (yr_day - corr_time(12)+365.25)/(corr_time(1)+365.25-corr_time(12));
-    else
-       id0 = find(yr_day>corr_time,1,'last');
-   id1 = id0+1;
-   cf  = (yr_day - corr_time(id0))/(corr_time(id1)-corr_time(id0));
-end
-
-swrd= swr;
-swr_cr = cf*swr_mult(:,:,id0) + (1-cf)*swr_mult(:,:,id1);
-swr= swr.*swr_cr;
-
-ncwrite(frcname,'swrad',swr,[1 1 irec]);
-ncwrite(frcname,'lwrad',lwr,[1 1 irec]);
-
-% ---- Absolute humidity -----
-
-t2m = get_frc_era(data,grd,'t2m',irec,'linear'); % 2 meter air temp [K]
-d2m = get_frc_era(data,grd,'d2m',irec,'linear'); % 2 meter air dew point temp [K]
-t2m = t2m - 273.15; % K to C
-d2m = d2m - 273.15; % K to C
-    Qair=(exp((17.625*d2m)./(243.04+d2m))./exp((17.625*t2m)./(243.04+t2m)));  % Relative humidity fraction
-
-    % Relative to absolute humidity assuming constand pressure
-
-    patm=1010.0; 
-
-    cff=(1.0007+3.46e-6*patm).*6.1121.*exp(17.502*t2m./(240.97+t2m));
-    cff=cff.* Qair;
-    qair =0.62197.*(cff./(patm-0.378.*cff)) ;
-
-ncwrite(frcname,'qair',qair,[1 1 irec]);
-ncwrite(frcname,'Tair',t2m, [1 1 irec]);
-
-% ---- Rain ------
-rain = get_frc_era(data,grd,'tp',irec,'linear'); % precipitation [m]
-
-% Translate to fluxes. ERA5 stores values integrated over 1 hour
-% Are these centered around the current time? 
-if 0 % output in m/s
-    rain = rain/3600;
-else % output in cm/day
-    rain = rain*100*24;
-end
-
-% ---- Correction to rain using Dai and Trenberth data  -----
-% Temporal interpolation in a monthly climatology
-if add_rivers
-    yr_day = mod(days,365.25);
-    if yr_day>= riv_time(12)
-        id0 = 12;
-        id1 =  1;
-        cf  = (yr_day -  riv_time(12))/( riv_time(1)+365.25- riv_time(12));
-    elseif yr_day<=riv_time(1)
-        id0 = 12;
-        id1 =  1;
-        cf  = (yr_day -  riv_time(12)+365.25)/( riv_time(1)+365.25- riv_time(12));
-    else
-        id0 = find(yr_day>riv_time,1,'last');
-        id1 = id0+1;
-        cf  = (yr_day -  riv_time(id0))/( riv_time(id1)- riv_time(id0));
+    create_frc_bulk(grdname,frcname);
+    
+    for irec = 1:nrecord
+        disp(['Record: ' num2str(irec)])
+        
+        % ---- time -----
+        time = ncread(datname,'time',[irec],[1]);
+        
+        % translate to days since 2000,1,1
+        days = double(time)/24. + datenum(1900,1,1) - datenum(2000,1,1) 
+        ncwrite(frcname,'time',days,[irec]);
+        
+        % ---- 10 meter winds -----
+        u = get_frc_era(data,grd,'u10',irec,'makima');
+        v = get_frc_era(data,grd,'v10',irec,'makima');
+        
+        % rotate to grid angles 
+        ugrid = cosa.*u + sina.*v;
+        vgrid =-sina.*u + cosa.*v;
+        
+        if wind_dropoff
+            ugrid = ugrid.*mult;
+            vgrid = vgrid.*mult;
+        end
+        
+        ncwrite(frcname,'uwnd',ugrid,[1 1 irec]);
+        ncwrite(frcname,'vwnd',vgrid,[1 1 irec]);
+        
+        % ---- Incoming Radiation -----
+        swr = get_frc_era(data,grd,'ssr',irec,'linear');  % downward_shortwave_flux [J/m2]
+        lwr = get_frc_era(data,grd,'strd',irec,'linear'); % downward_longwave_flux [J/m2]
+        
+        % Translate to fluxes. ERA5 stores values integrated over 1 hour
+        % Are these centered around the current time? 
+        swr = swr/3600;
+        lwr = lwr/3600;
+        
+        % ---- Correction to swr using  COREv2 dataset -----
+        % This is a multiplicative correcting, trying to account for 
+        % for errors in the ERA5 cloud cover
+        % temporal interpolation in a monthly climatology
+        yr_day = mod(days,365.25);
+        if yr_day>=corr_time(12)
+            id0 = 12;
+            id1 =  1;
+            cf  = (yr_day - corr_time(12))/(corr_time(1)+365.25-corr_time(12));
+        elseif yr_day<=corr_time(1)
+            id0 = 12;
+            id1 =  1;
+            cf  = (yr_day - corr_time(12)+365.25)/(corr_time(1)+365.25-corr_time(12));
+        else
+            id0 = find(yr_day>corr_time,1,'last');
+            id1 = id0+1;
+            cf  = (yr_day - corr_time(id0))/(corr_time(id1)-corr_time(id0));
+        end
+        
+        swrd= swr;
+        swr_cr = cf*swr_mult(:,:,id0) + (1-cf)*swr_mult(:,:,id1);
+        swr= swr.*swr_cr;
+        
+        ncwrite(frcname,'swrad',swr,[1 1 irec]);
+        ncwrite(frcname,'lwrad',lwr,[1 1 irec]);
+        
+        % ---- Absolute humidity -----
+        
+        t2m = get_frc_era(data,grd,'t2m',irec,'linear'); % 2 meter air temp [K]
+        d2m = get_frc_era(data,grd,'d2m',irec,'linear'); % 2 meter air dew point temp [K]
+        t2m = t2m - 273.15; % K to C
+        d2m = d2m - 273.15; % K to C
+        Qair=(exp((17.625*d2m)./(243.04+d2m))./exp((17.625*t2m)./(243.04+t2m)));  % Relative humidity fraction
+        
+        % Relative to absolute humidity assuming constand pressure
+        
+        patm=1010.0; 
+        
+        cff=(1.0007+3.46e-6*patm).*6.1121.*exp(17.502*t2m./(240.97+t2m));
+        cff=cff.* Qair;
+        qair =0.62197.*(cff./(patm-0.378.*cff)) ;
+        
+        ncwrite(frcname,'qair',qair,[1 1 irec]);
+        ncwrite(frcname,'Tair',t2m, [1 1 irec]);
+        
+        % ---- Rain ------
+        rain = get_frc_era(data,grd,'tp',irec,'linear'); % precipitation [m]
+        
+        % Translate to fluxes. ERA5 stores values integrated over 1 hour
+        % Are these centered around the current time? 
+        if 0 % output in m/s
+            rain = rain/3600;
+        else % output in cm/day
+            rain = rain*100*24;
+        end
+        
+        % ---- Correction to rain using Dai and Trenberth data  -----
+        % Temporal interpolation in a monthly climatology
+        if add_rivers
+            yr_day = mod(days,365.25);
+            if yr_day>= riv_time(12)
+                id0 = 12;
+                id1 =  1;
+                cf  = (yr_day -  riv_time(12))/( riv_time(1)+365.25- riv_time(12));
+            elseif yr_day<=riv_time(1)
+                id0 = 12;
+                id1 =  1;
+                cf  = (yr_day -  riv_time(12)+365.25)/( riv_time(1)+365.25- riv_time(12));
+            else
+                id0 = find(yr_day>riv_time,1,'last');
+                id1 = id0+1;
+                cf  = (yr_day -  riv_time(id0))/( riv_time(id1)- riv_time(id0));
+            end
+            rain = rain + cf*riv_flux(:,:,id0) + (1-cf)*riv_flux(:,:,id1);
+        end
+        
+        ncwrite(frcname,'rain',rain, [1 1 irec]);
+    
     end
-    rain = rain + cf*riv_flux(:,:,id0) + (1-cf)*riv_flux(:,:,id1);
-end
-
-ncwrite(frcname,'rain',rain, [1 1 irec]);
-
-end
 end % 
 
 
